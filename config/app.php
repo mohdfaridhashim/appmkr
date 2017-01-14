7fe25dcbf96b85e84bb4e3cbc1abc8b8600a6890
mohd farid hashim <zackseen@gmail.com>
2017-01-14 14:02:55 +0800

    enable log

--- config/app.php
+++ config/app.php
@@ -123,7 +123,7 @@ return [
     //'log' => env('APP_LOG', 'single'),
 	'log' => 'errorlog',
 
-    'log_level' => env('APP_LOG_LEVEL', 'debug'),
+    //'log_level' => env('APP_LOG_LEVEL', 'debug'),
 
     /*
     |--------------------------------------------------------------------------
